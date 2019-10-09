565549f77db0cd1492496debd8be0ea92b510d98
Oliver <onefasteuro@gmail.com>
2019-10-09 15:34:53 -0400

    added migration

--- src/ShopifyApp.php
+++ src/ShopifyApp.php
@@ -12,23 +12,6 @@ class ShopifyApp extends Model
 	//appends to the toArray output
 	protected $appends = ['shopify_shop_id'];
 	
-	public function getShopNameAttribute()
-	{
-		$exploded = explode('.', $this->shop);
-		return count($exploded) > 0 ? $exploded[0] : null;
-	}
-	
-	
-	public function getShopifyShopIdAttribute()
-	{
-		if(preg_match('/gid/', $this->shop_id)) {
-			return sprintf('gid://shopify/Shop/%d', $this->shop_id);
-		}
-		else {
-			return $this->shop_id;
-		}
-	}
-	
 	/**
 	 * @param string $domain
 	 * @param string $handle
